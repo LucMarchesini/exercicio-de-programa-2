87e761ca329c256b1095c7a798162222b1579573
Murilo Miacci <murilo.miacci@gmail.com>
2026-04-09 15:31:19 -0300

    Exercicio 4 Concluído

--- funcoes.py
+++ funcoes.py
@@ -15,3 +15,9 @@ def remover_dado(rolados, guardados, indice):
     rolados.append(guardados[indice])
     guardados.pop(indice)
     return [rolados, guardados]
+
+def calcula_pontos_regra_simples(lista):
+    dic = {}
+    conta1, conta2, conta3, conta4, conta5, conta6 = lista.count(1), lista.count(2), lista.count(3), lista.count(4), lista.count(5), lista.count(6)
+    dic[1], dic[2], dic[3], dic[4], dic[5], dic[6] = 1*conta1, 2*conta2, 3*conta3, 4*conta4, 5*conta5, 6*conta6
+    return dic
