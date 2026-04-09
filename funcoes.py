f54cbecc81ebc60d1b914ac69e4d6afd469c6575
Lucas Marchesini <lucassmarchesini@gmail.com>
2026-04-09 14:54:36 -0300

    Exercicio dois concluido

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,7 @@ def rolar_dados(dados):
         s = random.randint(1, 6)
         resultados.append(s)
     return resultados
+def guardar_dado(rolados, guardados, indice):
+    guardados.append(rolados[indice])
+    rolados.pop(indice)
+    return [rolados, guardados]
