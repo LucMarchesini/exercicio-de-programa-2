2abb737a4aca5279d27ddb596d2342f8a74687be
Murilo Miacci <murilo.miacci@gmail.com>
2026-04-09 15:15:11 -0300

    Exercício 3 Concluído (Sem Print)

--- funcoes.py
+++ funcoes.py
@@ -15,9 +15,3 @@ def remover_dado(rolados, guardados, indice):
     rolados.append(guardados[indice])
     guardados.pop(indice)
     return [rolados, guardados]
-
-dados_rolados = [2, 2, 2, 2]
-dados_no_estoque = [1]
-dado_para_remover = 0
-
-print(remover_dado(dados_rolados, dados_no_estoque, dado_para_remover))
