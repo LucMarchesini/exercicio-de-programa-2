832acf0bc4b12d69e84986882a784d7c886429b6
Lucas Marchesini <lucassmarchesini@gmail.com>
2026-04-09 15:03:15 -0300

    Exercicio tres completo

--- funcoes.py
+++ funcoes.py
@@ -5,7 +5,19 @@ def rolar_dados(dados):
         s = random.randint(1, 6)
         resultados.append(s)
     return resultados
+
 def guardar_dado(rolados, guardados, indice):
     guardados.append(rolados[indice])
     rolados.pop(indice)
     return [rolados, guardados]
+
+def remover_dado(rolados, guardados, indice):
+    rolados.append(guardados[indice])
+    guardados.pop(indice)
+    return [rolados, guardados]
+
+dados_rolados = [2, 2, 2, 2]
+dados_no_estoque = [1]
+dado_para_remover = 0
+
+print(remover_dado(dados_rolados, dados_no_estoque, dado_para_remover))
