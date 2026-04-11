38ada6af9c4dac69e57bb30424bfdecaa3983335
Murilo Miacci <murilo.miacci@gmail.com>
2026-04-10 23:43:43 -0300

    Opção de imprimir a cartela

--- programa.py
+++ programa.py
@@ -19,3 +19,52 @@ cartela_de_pontos = {
 
 imprime_cartela(cartela_de_pontos) # IMPRIME SEM PRINT
 rolados = rolar_dados(contador_para_rodar)
+
+while True: # Esse loop serve para verificar se a tabela esta completa e contempla o jogo inteiro
+    cartela_cheia = True
+
+    for chave1 in cartela_de_pontos["regra_simples"]:
+        if cartela_de_pontos["regra_simples"][chave1] == -1:
+            cartela_cheia = False
+    for chave2 in cartela_de_pontos["regra_avancada"]:
+        if cartela_de_pontos["regra_avancada"][chave2] == -1:
+            cartela_cheia = False
+
+    if cartela_cheia == True: # Acaba o jogo se nao tiver nenhuma vazia
+        break
+
+    print(f'Dados rolados: {rolados}')
+    print(f"Dados guardados: {guardados}")
+
+    print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:') # Solicitação para o usuário no print
+    while True: # Roda o loop enquanto o usuário não digita uma entrada válida (opção das listas)
+        usuario = input()
+        if usuario == '1':
+            print('Digite o índice do dado a ser guardado (0 a 4):')
+            indice_guardar = int(input())
+            contador_para_rodar -= 1
+            dados_guardados = guardar_dado(rolados, guardados, indice_guardar)
+            rolados = dados_guardados[0]
+            guardados = dados_guardados[1]
+            break
+
+        elif usuario == '2':
+            print("Digite o índice do dado a ser removido (0 a 4):")
+            indice_remover = int(input())
+            contador_para_rodar += 1
+            dados_removidos = remover_dado(rolados, guardados, indice_remover)
+            rolados = dados_removidos[0]
+            guardados = dados_removidos[1]
+            break
+
+        elif usuario == '3':
+            if count_rolagens < 2:
+                rolados = rolar_dados(contador_para_rodar)
+                count_rolagens += 1
+            else:
+                print('Você já usou todas as rerrolagens.')
+            break
+
+        elif usuario == '4':
+            imprime_cartela(cartela_de_pontos)
+            break
